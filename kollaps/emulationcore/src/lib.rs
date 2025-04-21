4ddfd97f7a2c0b2a6683ed40c793ee0e83a5bcce
Samim Umut Ökten <samim.oekten@unifr.ch>
2025-04-21 09:38:18 +0200

    Export modules used by the controller crate

---
+++ kollaps/emulationcore/src/lib.rs
@@ -0,0 +1,12 @@
+// This file exists to enable the library target
+// for the `controller` crate.
+pub mod state;
+pub mod xmlgraphparser;
+pub mod aux;
+mod emulationcore;
+mod graph;
+mod elements;
+mod emulation;
+mod eventscheduler;
+mod docker;
+mod communication;
