3772f0a4fee99f2fba2b340cbf3daed185011c73
Samim Umut Ökten <samim.oekten@unifr.ch>
2025-06-30 21:11:06 +0200

    Temporary fix for inversed source and destination address fields on network packets.
    More information:
    
    While re-implementing monitor to use aya-rs instead of redbpf, I chose
    to use `xdp` instead of `socket_filter` for the potential performance gain.
    
    But since `xdp` is attached to the virtual ethernet of a container, this
    caused the source and destination fields of packets being inverted.
    
    The correct fix should be to switch `xdp` for `socket_filter`.

--- kollaps/monitor-ebpf/src/main.rs
+++ kollaps/monitor-ebpf/src/main.rs
@@ -2,17 +2,18 @@
 #![no_main]
 
 use monitor_common::{Message, SocketAddr};
-use network_types::{ip::Ipv4Hdr, eth::{EthHdr, EtherType}};
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::Ipv4Hdr,
+};
 
 use aya_ebpf::{
+    bindings::xdp_action,
+    helpers::bpf_ktime_get_ns,
     macros::{map, xdp},
     maps::{HashMap, PerfEventArray},
     programs::XdpContext,
-    helpers::bpf_ktime_get_ns,
-    bindings::xdp_action
 };
-
-use core::mem;
 
 // Map to hold perf_events
 #[map]
@@ -26,11 +27,10 @@ static USAGE: HashMap<u32, u32> = HashMap::with_max_entries(4096, 0);
 #[map]
 static TIME: HashMap<u32, u64> = HashMap::with_max_entries(4096, 0);
 
-
 #[xdp]
 pub fn monitor(ctx: XdpContext) -> u32 {
     match try_measure_tcp_lifetime(ctx) {
-        Ok(_)  => xdp_action::XDP_PASS,
+        Ok(_) => xdp_action::XDP_PASS,
         Err(_) => xdp_action::XDP_PASS,
     }
 }
@@ -43,7 +43,14 @@ fn try_measure_tcp_lifetime(ctx: XdpContext) -> Result<(), i64> {
     }
 
     let ipv4hdr: *const Ipv4Hdr = unsafe { ptr_at(&ctx, EthHdr::LEN)? };
-    let dst = SocketAddr { addr: u32::from_be_bytes(unsafe { (*ipv4hdr).dst_addr }) };
+    // While rewriting monitor, I used `xdp` instead of `socket_filter` for the performance benefits.
+    // But since `xdp` gets attached to the virtual ethernet interface of a container,
+    // the source and target addresses of packets are inversed.
+    // TODO Switch xdp back to socket_filter (should require minimal change).
+    let dst = SocketAddr {
+        addr: u32::from_be_bytes(unsafe { (*ipv4hdr).src_addr }),
+    };
+
     let len: u32 = (ctx.data_end() - ctx.data()) as u32;
 
     unsafe {
@@ -52,25 +59,26 @@ fn try_measure_tcp_lifetime(ctx: XdpContext) -> Result<(), i64> {
         match TIME.get(&dst.addr) {
             None => {
                 TIME.insert(&dst.addr, &time, 0)?;
-            },
+            }
             Some(prev_time) => {
                 match USAGE.get(&dst.addr) {
                     None => {
                         USAGE.insert(&dst.addr, &len, 0)?;
-                    },
+                    }
                     Some(prev_len) => {
                         let new_len = prev_len + len;
 
                         if time - prev_time > 5_000_000 {
-                            let msg = Message { dst: dst.addr, bytes: new_len };
+                            let msg = Message {
+                                dst: dst.addr,
+                                bytes: new_len,
+                            };
                             PERF_EVENTS.output(&ctx, &msg, 0);
-                            // TODO consider changing accumulating bytes to
-                            // bytes since last send to the PERF_EVENTS and
-                            // reset it to zero. Currently there is a chance
-                            // that the USAGE value of u32 will overflow after
-                            // receiving 4GB of packets.
-                            // This will potentially require changes to the logic
-                            // in emulationcore as well.
+                            // TODO consider changing 'accumulating bytes' to
+                            // 'bytes since last send' to the PERF_EVENTS and reset it to zero.
+                            // As far as I understand, currently there is a chance
+                            // that the USAGE's u32 will overflow after receiving 4GB of packet lengths.
+                            // This will potentially require changes to the logic in emulationcore
                             USAGE.insert(&dst.addr, &new_len, 0)?;
                             TIME.insert(&dst.addr, &time, 0)?;
                         } else {
@@ -88,12 +96,10 @@ fn try_measure_tcp_lifetime(ctx: XdpContext) -> Result<(), i64> {
 // It performs bounds checking by comparing the desired memory range
 // (start + offset + len) against the end of the data (end).
 #[inline(always)]
-unsafe fn ptr_at<T>(
-    ctx: &XdpContext, offset: usize
-) -> Result<*const T, i64> {
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, i64> {
     let start = ctx.data();
     let end = ctx.data_end();
-    let len = mem::size_of::<T>();
+    let len = core::mem::size_of::<T>();
 
     if start + offset + len > end {
         return Err(-1);
@@ -111,4 +117,4 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[link_section = "license"]
 #[no_mangle]
-static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";  // TODO correct licence
