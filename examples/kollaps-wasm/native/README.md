bb46d5a7fcf796220082634ca15956209c9837a7
Samim Umut Ökten <samim.oekten@unifr.ch>
2025-12-09 13:06:37 +0100

    doc(wasm): implement examples for kollaps-wasm

---
+++ examples/kollaps-wasm/native/README.md
@@ -0,0 +1,3 @@
+# Running kollaps-wasm experiments with native programs
+The topology file imitates the iperf3 example for swarm/docker orchestrator deployments.
+It requires the `iperf3` executable to be available in PATH.
---
+++ examples/kollaps-wasm/wasm/simple-server/src/main.rs
@@ -0,0 +1,37 @@
+use std::env::args;
+use std::io::{copy, sink};
+use std::net::TcpListener;
+
+fn main() {
+    eprintln!("Starting client.");
+    let mut args = args();
+    let _prog = args.next();
+    let port = args
+        .next()
+        .and_then(|a| a.parse::<u32>().ok())
+        .unwrap_or(3000);
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = match TcpListener::bind(&addr) {
+        Ok(l) => l,
+        Err(e) => {
+            eprintln!("Failed to bind to {}: {}", addr, e);
+            std::process::exit(1);
+        }
+    };
+
+    eprintln!("Listening on {}", addr);
+
+    for stream in listener.incoming() {
+        if let Ok(mut s) = stream {
+            let peer_addr = s
+                .peer_addr()
+                .and_then(|a| Ok(a.to_string()))
+                .unwrap_or("<failed to get peer address>".to_string());
+            let mut sink = sink();
+            if let Err(e) = copy(&mut s, &mut sink) {
+                eprintln!("Error while reading from client {}: {}", peer_addr, e);
+            }
+            eprintln!("Stream for {} dropped.", peer_addr);
+        }
+    }
+}
